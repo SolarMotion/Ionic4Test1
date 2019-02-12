d4df1f23e9963c2d753e812ebe1bfed7702257e8
chien <ywc92@hotmail.com>
2019-02-12 22:45:45 +0800

    check in login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
     path: 'home',
+    canActivate: [AuthGuard],
     loadChildren: './home/home.module#HomePageModule'
   },
   {
     path: 'list',
+    canActivate: [AuthGuard],
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'login', loadChildren: './public/login/login.module#LoginPageModule' }
 ];
 
 @NgModule({
