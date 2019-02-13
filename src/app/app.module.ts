f05cbfcc931d3335df67089e2778443fa2029ce4
Wei Chien Yap <weichienyap@seednethq.seednet.com.my>
2019-02-13 16:18:14 +0800

    add loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { IonicStorageModule } from '@ionic/storage';
-import { Alert } from './shared/alert';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { IonicStorageModule } from "@ionic/storage";
+import { Alert } from "./shared/alert";
+import { Loader } from "./shared/loader";
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,14 +20,14 @@ import { Alert } from './shared/alert';
     IonicModule.forRoot(),
     AppRoutingModule,
     IonicStorageModule.forRoot(),
-    
   ],
   providers: [
     StatusBar,
     SplashScreen,
     Alert,
+    Loader,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/public/login/login.page.ts
+++ src/app/public/login/login.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, AfterViewInit } from "@angular/core";
 import { AuthenticationService } from "./../../services/authentication.service";
 import { Alert } from "./../../shared/alert";
+import { Loader } from "./../../shared/loader";
 
 @Component({
   selector: "app-login",
@@ -11,17 +12,24 @@ export class LoginPage implements OnInit {
   constructor(
     private authService: AuthenticationService,
     private alertBox: Alert,
+    private loaderBox: Loader
   ) {}
 
   ngOnInit() {}
 
+  ionViewDidLoad() {}
   loginUser() {
     let isLoginSuccess = false;
 
     // Call authentication web service here
 
     if (!isLoginSuccess) {
-      this.alertBox.show("Failed", "Test", ["OK"]);
+      // this.alertBox.show("Failed", "Test", ["OK"]);
+      this.loaderBox.present();
+
+      setTimeout(() => {
+        this.loaderBox.dismiss();
+    }, 5000);
     } else {
       this.authService.login();
     }
